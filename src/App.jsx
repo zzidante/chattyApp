08f1b1e5630d59c2dd67cbf287c8f724f18e6ef6
Mel <zzidante@gmail.com>
2017-11-01 18:27:52 +0000

    Split html into React components and added js object to act as db

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 import React, {Component} from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Hello React :)</h1>
-    );
+import Chatbar from './Chatbar.jsx';
+import MessageList from './MessageList.jsx';
+
+
+  class App extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        currentUser: {name: 'Freddy'},
+        messages: [
+          {
+            username: 'Cheese',
+            content: 'Has anyone seen my marbles?',
+          },
+          {
+            username: 'Lettuce',
+            content: 'No, I think you lost them. You lost your marbles Cheese. You lost them for good.'
+          }
+        ]
+      };
+    }
+
+    
+
+
+    render() {
+      return (
+        <div>
+          <nav className="navbar">
+            <a href="/" className="navbar-brand">Chatty</a>
+          </nav>
+            <MessageList />
+            <Chatbar currentUser = { this.state.currentUser } />
+        </div>
+      );
+    }
   }
-}
+
 export default App;
