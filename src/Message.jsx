6f417713bdfed795cffd827454af9277ed0f47bc
Mel <zzidante@gmail.com>
2017-11-03 22:13:35 +0000

    Notification messages now only add their div when a notification exists

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,17 +3,23 @@ import React , {Component} from 'react';
 class Message extends Component {
   render() {
     const message = this.props;
+
+    if(message.notification) {
+      return ( 
+      <div className="message system">
+          { message.notification }
+        </div>
+      )
+    }
+
     return (
       <div>
         <div className="message">
           <span className="message-username">{ message.username }</span>
           <span className="message-content">{ message.content }</span>
         </div>
-
-        <div className="message system">
-          { message.notification }
-        </div>
       </div>
+
     )
   }
 }
